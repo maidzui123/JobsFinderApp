9361adee46c538b658ffbaf4920d889b66c17db3
maidzui123 <maiduy190802@gmail.com>
2022-11-16 22:32:51 +0700

    small upd

--- app/src/main/java/com/example/jobfinderapp/BottomSheet.java
+++ app/src/main/java/com/example/jobfinderapp/BottomSheet.java
@@ -15,24 +15,22 @@ import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class BottomSheet extends BottomSheetDialogFragment {
-    public BottomSheet() {
-    }
     String[] job_items = {"Programming", "Marketing", "Design", "Healthcare"};
     AutoCompleteTextView autoCompleteTxt;
-    @Nullable
+    private View mView;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View mView = inflater.inflate(R.layout.bottom_sheet_dialog,container, false);
-        autoCompleteTxt = mView.findViewById(R.id.autoCompleteTxt);
-        ArrayAdapter<String> adapterItems = new ArrayAdapter<String>(getActivity(), R.layout.job_dropdown_item, job_items);
-        autoCompleteTxt.setAdapter(adapterItems);
-//        autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                String item = parent.getItemAtPosition(position).toString();
-//                Toast.makeText(getActivity(), "Item: " + item, Toast.LENGTH_SHORT).show();
-//            }
-//        });
-        return mView;
+         mView = inflater.inflate(R.layout.bottom_sheet_dialog,container, false);
+         autoCompleteTxt = mView.findViewById(R.id.autoCompleteTxt);
+         ArrayAdapter<String> adapterItems = new ArrayAdapter<String>(getActivity(), R.layout.job_dropdown_item, job_items);
+         autoCompleteTxt.setAdapter(adapterItems);
+         autoCompleteTxt.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getActivity(), "Item: " + item, Toast.LENGTH_SHORT).show();
+            }
+         });
+         return mView;
     }
 }
